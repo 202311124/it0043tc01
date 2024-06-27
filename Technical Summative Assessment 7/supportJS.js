6ec8d9342b2704d9fc3d97e6819c0be0a0b8e4de
202311124 <167260919+202311124@users.noreply.github.com>
2024-06-28 00:23:13 +0800

    Add files via upload

--- Technical Summative Assessment 7/supportJS.js
+++ Technical Summative Assessment 7/supportJS.js
@@ -1,11 +1,68 @@
-let flex = 1
+function reset(){
+    document.querySelector('.display').style.gap = "0px";
 
-function flex(i){
-    if (flex == 1){
-        c.style.flexdirection: row;
+    document.querySelector('.display').style.flexDirection = "row";
+
+    document.querySelector('.display').style.justifyContent = "flex-start";
+
+    document.querySelector('.display').style.alignItems = "stretch";
+    
+    let grow1 = 0;
+    let grow2 = 0;
+    let grow3 = 0;
+    document.querySelector('.box1').style.flexGrow = grow1;
+    document.querySelector('.box2').style.flexGrow = grow2;
+    document.querySelector('.box3').style.flexGrow = grow3;
+}
+
+function setgap(){
+    let gap = document.getElementById('gap').value;
+    document.querySelector('.display').style.gap = gap+"px";
+}
+
+function flex(direction){
+    document.querySelector('.display').style.flexDirection = direction;
+}
+
+function justify(content){
+    document.querySelector('.display').style.justifyContent = content;
+}
+
+function align(items){
+    document.querySelector('.display').style.alignItems = items;
+}
+
+function setgrow(growbox){
+    if (growbox == 1){
+        let grow1 = document.getElementById('grow1').value;
+        document.querySelector('.box1').style.flexGrow = grow1;
     }
 
-    else {
-        c.style.flexdirection: column;
+    else if (growbox == 2){
+        let grow2 = document.getElementById('grow2').value;
+        document.querySelector('.box2').style.flexGrow = grow2;
+    }
+
+    else if (growbox == 3){
+        let grow3 = document.getElementById('grow3').value;
+        document.querySelector('.box3').style.flexGrow = grow3;
+    }
+
+    else if (growbox == 0){
+        let grow1 = document.getElementById('grow1').value;
+        let grow2 = document.getElementById('grow2').value;
+        let grow3 = document.getElementById('grow3').value;
+        document.querySelector('.box1').style.flexGrow = grow1;
+        document.querySelector('.box2').style.flexGrow = grow2;
+        document.querySelector('.box3').style.flexGrow = grow3;
+    }
+
+    else if (growbox == 4){
+        let grow1 = 0;
+        let grow2 = 0;
+        let grow3 = 0;
+        document.querySelector('.box1').style.flexGrow = grow1;
+        document.querySelector('.box2').style.flexGrow = grow2;
+        document.querySelector('.box3').style.flexGrow = grow3;
     }
 }
